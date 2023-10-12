29da866bd18423bb5d3848b055a756fb31405b32
Ghukii <pringles4823@gmail.com>
2023-10-12 23:23:08 +0300

    1

--- lesson_1_5/Пароли.cpp
+++ lesson_1_5/Пароли.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
+    string pw;
+
+    cin>>pw;
+
+    bool ASCII = true;
+
+    for(size_t i = 0; i != pw.size(); ++i){
+        if(!(33 <= int(pw[i]) <= 126)){
+            cout<<"NO";
+            return 0;
+        }
+    }
+
+    int big = 0, small = 0, numbers = 0, spec = 0;
 }
