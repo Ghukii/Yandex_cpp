7f756be20d60aa6f9518175b60639c16635d7071
Ghukii <pringles4823@gmail.com>
2023-10-13 13:53:15 +0300

    22

--- lesson_1_5/Пароли.cpp
+++ lesson_1_5/Пароли.cpp
@@ -7,8 +7,6 @@ int main(){
 
     cin>>pw;
 
-    bool ASCII = true;
-
     for(size_t i = 0; i != pw.size(); ++i){
         if(!(33 <= int(pw[i]) <= 126)){
             cout<<"NO";
@@ -17,4 +15,19 @@ int main(){
     }
 
     int big = 0, small = 0, numbers = 0, spec = 0;
+
+    for(size_t i = 0; i != pw.size(); ++i){
+        int ch = (int) pw[i];
+
+        if(33 <= ch <= 47){spec++;}
+        if(48 <= ch <= 57){numbers++;}
+        if(58 <= ch <= 64){spec++;}
+        if(65 <= ch <= 90){big++;}
+        if(91 <= ch <= 96){spec++;}
+        if(97 <= ch <= 122){small++;}
+        if(123 <= ch <= 126){spec++;}
+    }
+
+    if(big >= 3 || small >= 3 || numbers >= 3 || spec >= 3){cout<<"YES";}
+    else {cout<<"NO";}
 }
