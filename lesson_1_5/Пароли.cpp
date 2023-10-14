3df074deaa19d3ed45d5094116849312005327d6
Ghukii <pringles4823@gmail.com>
2023-10-14 05:18:46 +0300

    Soudex added

--- lesson_1_5/Пароли.cpp
+++ lesson_1_5/Пароли.cpp
@@ -7,8 +7,13 @@ int main(){
 
     cin>>pw;
 
+    if(pw.length() < 8  || pw.length() > 14){
+        cout<<"NO";
+        return 0;
+    }
+
     for(size_t i = 0; i != pw.size(); ++i){
-        if(!(33 <= int(pw[i]) <= 126)){
+        if(!(33 <= (int) pw[i] && (int) pw[i] <= 126)){
             cout<<"NO";
             return 0;
         }
@@ -19,13 +24,13 @@ int main(){
     for(size_t i = 0; i != pw.size(); ++i){
         int ch = (int) pw[i];
 
-        if(33 <= ch <= 47){spec++;}
-        if(48 <= ch <= 57){numbers++;}
-        if(58 <= ch <= 64){spec++;}
-        if(65 <= ch <= 90){big++;}
-        if(91 <= ch <= 96){spec++;}
-        if(97 <= ch <= 122){small++;}
-        if(123 <= ch <= 126){spec++;}
+        if(33 <= ch && ch <= 47){spec++;}
+        if(48 <= ch && ch <= 57){numbers++;}
+        if(58 <= ch && ch <= 64){spec++;}
+        if(65 <= ch && ch <= 90){big++;}
+        if(91 <= ch && ch <= 96){spec++;}
+        if(97 <= ch && ch <= 122){small++;}
+        if(123 <= ch && ch <= 126){spec++;}
     }
 
     if(big >= 3 || small >= 3 || numbers >= 3 || spec >= 3){cout<<"YES";}
