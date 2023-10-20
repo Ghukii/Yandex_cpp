0e11ec8ada3b658e242ee3f2f641258954c2aef5
Ghukii <pringles4823@gmail.com>
2023-10-20 17:46:50 +0300

    q11

--- lesson_1_5/Обратная_перестановка.cpp
+++ lesson_1_5/Обратная_перестановка.cpp
@@ -2,31 +2,25 @@
 #include<vector>
 using namespace std;
 
+void finnd(int x, vector<int> a){
+    for(size_t i = 0; i != a.size(); ++i){
+        if(a[i] == x)
+            cout<< (int) i + 1 << " ";
+    }
+}
+
 int main(){
     int n;
     cin >> n;
     vector<int> a;
-    vector<int> k;
 
     int x;
     for(int i=0; i < n; i++){
         cin>>x;
         a.push_back(x);
-        k.push_back(i + 1);
     }
 
-    for(size_t i = 0; i != a.size(); ++i){
-        for(size_t j = 0; j != a.size() - 1; ++j){
-            if(a[j] > a[j + 1]){
-                swap(a[j], a[j + 1]);
-                swap(k[j], k[j + 1]);
-            }
-        }
+    for(int i = 1; i < n + 1; i++){
+        finnd(i, a);
     }
-
-    for(size_t i = 0; i != a.size(); ++i){
-        cout<<k[i]<<" ";
-    }
-
-
 }
